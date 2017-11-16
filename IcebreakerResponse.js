761be0f448caa5448d9651645e8d7f5c7f1f2285
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-16 10:05:25 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,8 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
-static IcebreakerResponse.BatchCreate = function(arr){
-  return arr.map(function(mail){
+static IcebreakerResponse.BatchCreate = function(emails){
+  return emails.map(function(mail){
     return new IceBreakerResponse(mail[0])
   })
-} 
+}
